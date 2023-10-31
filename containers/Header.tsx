cbf88c8f2be057b1bf3660cd6a9ca68607e10b9c
Asilex-FR <149508237+Asilex-FR@users.noreply.github.com>
2023-10-31 15:44:37 +0000

    j'ai fait une modif css dans le header

--- containers/Header.tsx
+++ containers/Header.tsx
@@ -13,8 +13,8 @@ function Header() {
     <>
       <div className="ellipse"/>
         <div style={{width: '100%', height: '100%', position: 'relative'}}>
-      <div style={{width: 300, height: 145, left: 0, top: 0, position: 'absolute', background: '#008000', borderRadius: 9999}} />
-      <div style={{left: 17, top: 44, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 48, fontFamily: 'Inria Sans', fontWeight: '700', wordWrap: 'break-word'}}>Jacquemus X Konbini</div>
+      <div style={{width: 570, height: 80,right:508, top: 500, position: 'absolute', background: '#008000', borderRadius: 9999}} />
+      <div style={{left: 0,right:70, top: 500, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 48, fontFamily: 'Inria Sans', fontWeight: '700', wordWrap: 'break-word'}}>Jacquemus X Konbini</div>
       </div>
       
       <div>
--- pages/women.tsx
+++ pages/women.tsx
@@ -6,6 +6,7 @@ function women() {
   return (
     <>
       <h2>women</h2>
+      <h3>Bonjour</h3>
       <div>
         <HeaderWomen />
         <ProductList products={mockProducts}/>
